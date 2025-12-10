64c9ebf67f9918919a031f0f76697a30bfb7a567
Enrico <e.giacomazzi@contralto-audio.com>
2025-12-10 10:03:28 +0100

    fix cors

--- plugins/10-cors.js
+++ plugins/10-cors.js
@@ -1,5 +1,8 @@
 import fastifyCors from "@fastify/cors";
 
 export default async function(app, opts) {
-    await app.register(fastifyCors);
+    await app.register(fastifyCors, {
+        origin: '*',
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+    });
 }
